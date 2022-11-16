c728656594e195fd460b7ff132ba6d6c619c942a
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-11-16 19:23:45 +0100

    template updated with new page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import Nav from "./components/Nav";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact" element={<ContactPage />} />
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
         </main>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,9 @@ import { NavLink } from "react-router-dom";
 export default function Nav() {
     return (
         <nav>
-            <NavLink to="/" end>
-                Home
-            </NavLink>
+            <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
         </nav>
     );
 }
